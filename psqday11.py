7177c55794af61a0a6b6b2de2fe33fb62edacdb5
Mohit Kumar <mk5972345@gmail.com>
2026-04-03 19:03:43 +0530

    psqday10

---
+++ psqday11.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+print(type(arr))
+print(len(arr))
+
+import numpy as np
+a1D = np.array([1, 2, 3, 4])
+
+a2D = np.array([[1, 2], [3, 4]])
+
+a3D = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+
+print(a1D)
+print(a2D)
+print(a3D)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,12 @@
-dict["city"] = "giridih"
+# import numpy as np
+
+# a = np.array([[2,5],[7,8]])
+# print(a*7)
+
+import numpy as np
+
+arr = np.array([127, 128, 129], dtype= np.int8)
+
+print(arr)
+
+print("dtype:", arr.dtype)

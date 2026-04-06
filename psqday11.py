576066e1d14208ff8e8aeb1111f148b87227f600
Mohit Kumar <mk5972345@gmail.com>
2026-04-06 19:15:31 +0530

    psqday11

--- psqday11.py
+++ psqday11.py
@@ -1,18 +1,38 @@
+# import numpy as np
+
+# arr = np.array([1, 2, 3, 4, 5])
+
+# print(arr)
+# print(type(arr))
+# print(len(arr))
+
+# import numpy as np
+# a1D = np.array([1, 2, 3, 4])
+
+# a2D = np.array([[1, 2], 
+#                 [3, 4]])
+
+# a3D = np.array([[[1, 2], 
+#                   [3, 4]], 
+#                   [[5, 6], 
+#                   [7, 8]
+#                       ]])
+
+# print(a1D)
+# print(a2D)
+# print(a3D)
+
+# Create an array with 5 dimensions and verify that it has 5 dimensions:
+
+
 import numpy as np
+import time
+start = time.time()
 
-arr = np.array([1, 2, 3, 4, 5])
-
+arr = np.array([1, 2, 3, 4], ndmin=5)
 print(arr)
-print(type(arr))
-print(len(arr))
+print('number of dimensions :', arr.ndim)
 
-import numpy as np
-a1D = np.array([1, 2, 3, 4])
+end = time.time()
+print("Execution time :", end - start)
 
-a2D = np.array([[1, 2], [3, 4]])
-
-a3D = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
-
-print(a1D)
-print(a2D)
-print(a3D)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -5,8 +5,31 @@
 
 import numpy as np
 
-arr = np.array([127, 128, 129], dtype= np.int8)
+import pandas as pd
 
-print(arr)
+# # Arrays
+# a = np.array([[2,3,4,1],
+#               [4,2,3,1]])
+# b = np.array([[2,1,3,2],
+#               [3,2,4,1]])
 
-print("dtype:", arr.dtype)
+# # Square matrix
+# c = np.array([[2,3],
+#               [4,1]])
+
+# print("Dot Product:\n",      np.dot(a, b.T))
+# print("Matrix Multiply:\n",  np.matmul(a, b.T))
+# print("Inverse:\n",          np.linalg.inv(c))
+# print("Determinant:\n",      np.linalg.det(c))
+
+# val, vec = np.linalg.eig(c)
+# print("Eigenvalues:\n",      val)
+# print("Eigenvectors:\n",     vec)
+
+students_details = {"Name" : ['Mohit', 'Munna', 'Amit', 'Manoj', 'Roshan'],
+                    "Age" : [21,19,20,22,12],
+                    "City" : ['Delhi', 'Mumbai', 'jharkhand', 'Giridih', 'Pindatand']}
+print(students_details)
+
+d1 = pd.DataFrame(students_details)
+print(d1)
